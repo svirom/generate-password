0617129141eb4e58b48c19c8b91b0400376fb680
svirom <svirom@yahoo.com>
2019-10-02 16:36:15 +0300

    checkboxes ascii

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,15 @@ const rangeSlider = document.getElementById('pass-range-slider');
 const marginMin = document.getElementById('slider-margin-value-min');
 const marginMax = document.getElementById('slider-margin-value-max');
 
+const lowercaseArr = ['a', 'b',  'c',  'd',  'e',  'f',  'g',  'h',  'i',  'j',  'k',  'l',  'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x',  'y',  'z'];
+
+const uppercaseArr = ['A', 'B',  'C',  'D',  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L',  'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T',  'U',  'V',  'W',  'X',  'Y',  'Z'];
+
+const numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+const asciiArr = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '-', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
+// initiate range slider (amount of characters in password)
 noUiSlider.create(rangeSlider, {
   start: [8, 24],
   connect: true,
@@ -11,7 +20,7 @@ noUiSlider.create(rangeSlider, {
     'max': 24
   }
 });
-
+// add numbers of min and max amount of password characters
 rangeSlider.noUiSlider.on('update', function (values, handle) {
   if (handle) {
     marginMax.innerHTML = Number(values[handle]);
@@ -20,43 +29,39 @@ rangeSlider.noUiSlider.on('update', function (values, handle) {
   }
 });
 
-
-const lowercaseArr = ['a', 'b',  'c',  'd',  'e',  'f',  'g',  'h',  'i',  'j',  'k',  'l',  'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x',  'y',  'z'];
-
-const uppercaseArr = ['A', 'B',  'C',  'D',  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L',  'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T',  'U',  'V',  'W',  'X',  'Y',  'Z'];
-
-const numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-
-const asciiArr = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '-', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-
 document.addEventListener('DOMContentLoaded', addASCII);
-
+document.querySelector('.pass-ascii').addEventListener('click', labelActive);
 document.getElementById('generate').addEventListener('click', generatePass);
 
+// add ASCII characters checkboxes
 function addASCII() {
   for (let i = 0; i < asciiArr.length; i++) {
     let elASCII = document.createElement('span');
     elASCII.classList.add('ascii-element');
     elASCII.innerHTML = `
       <label for="el${asciiArr[i]}">${asciiArr[i]}</label>
-      <input type="checkbox" name="" id="el${asciiArr[i]}">
+      <input type="checkbox" name="" id="el${asciiArr[i]}" checked>
       <span></span>
     `;
     document.querySelector('.pass-ascii-separate').appendChild(elASCII);
   }
 }
 
-document.querySelector('.pass-ascii-all').addEventListener('click', labelActive);
-
+// toggle ASCII checkbox class "active"
 function labelActive(event) {
   if (event.target == this.querySelector('#ascii-all')) {
     // console.log(event.target);
-    this.querySelector('label').classList.toggle('active');
+    this.querySelector('.ascii-all-label').classList.toggle('active');
+    // enable/disable single ascii elements
+    const singleArr = this.querySelectorAll('.ascii-element');
+    singleArr.forEach( (elem) => {
+      elem.classList.toggle('active');
+    })
   }
 }
 
+// main function of generating password
 function generatePass() {
-
   // initiate amount of elements in password (according to password range data)
   const passMin = Number(document.querySelector('.noUi-handle-lower').getAttribute('aria-valuenow'));
   const passMax = Number(document.querySelector('.noUi-handle-upper').getAttribute('aria-valuenow'));
