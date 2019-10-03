d4b1e538104c27f99762adf418e03124d8b51d47
svirom <svirom@yahoo.com>
2019-10-03 17:06:13 +0300

    working with checkboxes groups

--- app/js/app.js
+++ app/js/app.js
@@ -29,15 +29,18 @@ rangeSlider.noUiSlider.on('update', function (values, handle) {
   }
 });
 
-document.addEventListener('DOMContentLoaded', addASCII);
-document.querySelector('.pass-ascii').addEventListener('click', labelActive);
+document.addEventListener('DOMContentLoaded', addCharacters);
+document.querySelector('.section-characters').addEventListener('click', labelActive);
 document.getElementById('generate').addEventListener('click', generatePass);
 
-// add ASCII characters checkboxes
-function addASCII() {
+// add characters checkboxes
+function addCharacters() {
+  // add ASCII characters checkboxes
   for (let i = 0; i < asciiArr.length; i++) {
     let elASCII = document.createElement('span');
+    elASCII.classList.add('character-element');
     elASCII.classList.add('ascii-element');
+    elASCII.classList.add('active');
     elASCII.innerHTML = `
       <label for="el${asciiArr[i]}">${asciiArr[i]}</label>
       <input type="checkbox" name="" id="el${asciiArr[i]}" checked>
@@ -45,21 +48,65 @@ function addASCII() {
     `;
     document.querySelector('.pass-ascii-separate').appendChild(elASCII);
   }
+  // add lowercase characters checkboxes
+  for (let i = 0; i < lowercaseArr.length; i++) {
+    let elLowercase = document.createElement('span');
+    elLowercase.classList.add('character-element');
+    elLowercase.classList.add('lower-element');
+    elLowercase.classList.add('active');
+    elLowercase.innerHTML = `
+      <label for="el${lowercaseArr[i]}">${lowercaseArr[i]}</label>
+      <input type="checkbox" name="" id="el${lowercaseArr[i]}" checked>
+      <span></span>
+    `;
+    document.querySelector('.pass-lower-separate').appendChild(elLowercase);
+  }
+  // add uppercase characters checkboxes
+  for (let i = 0; i < uppercaseArr.length; i++) {
+    let elUppercase = document.createElement('span');
+    elUppercase.classList.add('character-element');
+    elUppercase.classList.add('upper-element');
+    elUppercase.classList.add('active');
+    elUppercase.innerHTML = `
+      <label for="el${uppercaseArr[i]}">${uppercaseArr[i]}</label>
+      <input type="checkbox" name="" id="el${uppercaseArr[i]}" checked>
+      <span></span>
+    `;
+    document.querySelector('.pass-upper-separate').appendChild(elUppercase);
+  }
 }
 
-// toggle ASCII checkbox class "active"
+// toggle lowercase/uppercase/ASCII checkbox class "active"
 function labelActive(event) {
   if (event.target == this.querySelector('#ascii-all')) {
     // console.log(event.target);
-    this.querySelector('.ascii-all-label').classList.toggle('active');
+    this.querySelector('label[for="ascii-all"]').classList.toggle('active');
     // enable/disable single ascii elements
     const singleArr = this.querySelectorAll('.ascii-element');
     singleArr.forEach( (elem) => {
       elem.classList.toggle('active');
     })
   }
+  if (event.target == this.querySelector('#lower-all')) {
+    // console.log(event.target);
+    this.querySelector('label[for="lower-all"]').classList.toggle('active');
+    // enable/disable single ascii elements
+    const singleArr = this.querySelectorAll('.lower-element');
+    singleArr.forEach( (elem) => {
+      elem.classList.toggle('active');
+    })
+  }
+  if (event.target == this.querySelector('#upper-all')) {
+    // console.log(event.target);
+    this.querySelector('label[for="upper-all"]').classList.toggle('active');
+    // enable/disable single ascii elements
+    const singleArr = this.querySelectorAll('.upper-element');
+    singleArr.forEach( (elem) => {
+      elem.classList.toggle('active');
+    })
+  }
 }
-
+let result;
 // main function of generating password
 function generatePass() {
   // initiate amount of elements in password (according to password range data)
@@ -79,7 +126,6 @@ function generatePass() {
 
   console.log(passArr);
   let pass = passArr.join('');
-
-  document.querySelector('.pass-result span').innerHTML = `${pass} (${symbolsPass} symbols)`;
-
+ 
+  document.querySelector('.pass-result span').textContent = `${pass} (end)`;
 }
