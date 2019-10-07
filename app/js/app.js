5d6701ecfc07fa1ec6943156f0574b5b18be4e7b
svirom <svirom@yahoo.com>
2019-10-07 16:51:02 +0300

    generating logic

--- app/js/app.js
+++ app/js/app.js
@@ -11,14 +11,19 @@ const numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 const asciiArr = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '-', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
+let newLowercaseArr = [];
+let newUppercaseArr = [];
+let newNumberArr = [];
+let newAsciiArr = [];
+
 // initiate range slider (amount of characters in password)
 noUiSlider.create(rangeSlider, {
-  start: [8, 24],
+  start: [8, 32],
   connect: true,
   step: 1,
   range: {
     'min': 8,
-    'max': 24
+    'max': 32
   }
 });
 // add numbers of min and max amount of password characters
@@ -35,6 +40,7 @@ function Characters(arr, type) {
   this.arr = arr;
   this.type = type;
 }
+// add characters list of checkboxes
 Characters.prototype.addList = function() {
   for (let i = 0; i < this.arr.length; i++) {
     let element = document.createElement('span');
@@ -48,7 +54,48 @@ Characters.prototype.addList = function() {
     `;
     document.querySelector(`.pass-${this.type}-separate`).appendChild(element);
   }
-} 
+}
+// toggle "active" class in checkboxes
+Characters.prototype.changeActive = function() {
+  if (event.target == document.querySelector(`#${this.type}-all`)) {
+    // console.log(event.target);
+    document.querySelector(`label[for="${this.type}-all"]`).classList.toggle('active');
+    // enable/disable single character elements
+    const singleArr = document.querySelectorAll(`.${this.type}-element`);
+    singleArr.forEach( (elem) => {
+      elem.classList.toggle('active');
+    })
+  }
+}
+// return checked checkboxes array for password generator
+Characters.prototype.checkedArr = function() {
+  const singleArr = document.querySelectorAll(`.${this.type}-element`);
+  singleArr.forEach( (elem) => {
+    const elemText = elem.querySelector('label').textContent;
+    if ( document.querySelector(`#${this.type}-all`).checked == true ) {
+      if ( elem.querySelector('input[type="checkbox"]').checked == true ) {
+        if (this.type == 'lower') {
+          newLowercaseArr.push(elemText);
+        }
+        if (this.type == 'upper') {
+          newUppercaseArr.push(elemText);
+        }
+        if (this.type == 'number') {
+          newNumberArr.push(elemText);
+        }
+        if (this.type == 'ascii') {
+          newAsciiArr.push(elemText);
+        }
+      }
+    }
+  })
+}
+
+// create characters
+const lowercase = new Characters(lowercaseArr, 'lower');
+const uppercase = new Characters(uppercaseArr, 'upper');
+const number = new Characters(numberArr, 'number');
+const ascii = new Characters(asciiArr, 'ascii');
 
 document.addEventListener('DOMContentLoaded', addCharacters);
 document.querySelector('.section-characters').addEventListener('click', labelActive);
@@ -56,74 +103,43 @@ document.getElementById('generate').addEventListener('click', generatePass);
 
 // add characters checkboxes
 function addCharacters() {
-  // add lowercase characters checkboxes
-  const lowercase = new Characters(lowercaseArr, 'lower');
   lowercase.addList();
-  
-  // add uppercase characters checkboxes
-  const uppercase = new Characters(uppercaseArr, 'upper');
   uppercase.addList();
-  
-  // add number characters checkboxes
-  const number = new Characters(numberArr, 'number');
   number.addList();
-
-  // add ASCII characters checkboxes
-  const ascii = new Characters(asciiArr, 'ascii');
   ascii.addList();
 }
 
-// toggle lowercase/uppercase/ASCII checkbox class "active"
+// toggle lowercase/uppercase/number/ASCII checkbox class "active"
 function labelActive(event) {
-  if (event.target == this.querySelector('#lower-all')) {
-    // console.log(event.target);
-    this.querySelector('label[for="lower-all"]').classList.toggle('active');
-    // enable/disable single lowercase elements
-    const singleArr = this.querySelectorAll('.lower-element');
-    singleArr.forEach( (elem) => {
-      elem.classList.toggle('active');
-    })
-  }
-  if (event.target == this.querySelector('#upper-all')) {
-    // console.log(event.target);
-    this.querySelector('label[for="upper-all"]').classList.toggle('active');
-    // enable/disable single uppercase elements
-    const singleArr = this.querySelectorAll('.upper-element');
-    singleArr.forEach( (elem) => {
-      elem.classList.toggle('active');
-    })
-  }
-  if (event.target == this.querySelector('#number-all')) {
-    // console.log(event.target);
-    this.querySelector('label[for="number-all"]').classList.toggle('active');
-    // enable/disable single number elements
-    const singleArr = this.querySelectorAll('.number-element');
-    singleArr.forEach( (elem) => {
-      elem.classList.toggle('active');
-    })
-  }
-  if (event.target == this.querySelector('#ascii-all')) {
-    // console.log(event.target);
-    this.querySelector('label[for="ascii-all"]').classList.toggle('active');
-    // enable/disable single ascii elements
-    const singleArr = this.querySelectorAll('.ascii-element');
-    singleArr.forEach( (elem) => {
-      elem.classList.toggle('active');
-    })
-  }
+  lowercase.changeActive(event);
+  uppercase.changeActive(event);
+  number.changeActive(event);
+  ascii.changeActive(event);
 }
-let result;
+
 // main function of generating password
 function generatePass() {
   // initiate amount of elements in password (according to password range data)
   const passMin = Number(document.querySelector('.noUi-handle-lower').getAttribute('aria-valuenow'));
   const passMax = Number(document.querySelector('.noUi-handle-upper').getAttribute('aria-valuenow'));
   const symbolsPass = Math.floor(Math.random()*(passMax - passMin + 1)) + passMin;
-  console.log(symbolsPass);
+  // initiate empty arrays for checked characters
+  newLowercaseArr = [];
+  newUppercaseArr = [];
+  newNumberArr = [];
+  newAsciiArr = [];
   
+  // create new character arrays from only checked
+  lowercase.checkedArr();
+  uppercase.checkedArr();
+  number.checkedArr();
+  ascii.checkedArr();
+
   // generate password
   const passArr = [];
-  const arr = [...lowercaseArr, ...uppercaseArr, ...numberArr, ...asciiArr];
+  // const arr = [...lowercaseArr, ...uppercaseArr, ...numberArr, ...asciiArr];
+  const arr = [...newLowercaseArr, ...newUppercaseArr, ...newNumberArr, ...newAsciiArr];
+  // console.log(arr);
 
   for (let i = 1; i <= symbolsPass; i++) {
     const randomItem = arr[Math.floor(Math.random()*arr.length)];
@@ -133,5 +149,5 @@ function generatePass() {
   console.log(passArr);
   let pass = passArr.join('');
  
-  document.querySelector('.pass-result span').textContent = `${pass} (end)`;
+  document.querySelector('.pass-result span').textContent = `${pass} (${symbolsPass})`;
 }
