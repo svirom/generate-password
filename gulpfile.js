019c4465ef9a3568b9bf4385145aaf73fc2e0e18
svirom <svirom@yahoo.com>
2020-03-19 16:38:13 +0200

    gulp improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat'); // concat files
 var terser = require('gulp-terser'); // minify js files
@@ -8,7 +9,15 @@ var del = require('del');
 var htmlreplace = require('gulp-html-replace');
 var rename = require('gulp-rename');
 
-
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		port: 8080,
+		//notify: false
+	});
+});
 
 gulp.task('clean', function() { // clean all files in dist folder
   return del(['dist/**', '!dist']);
@@ -16,11 +25,13 @@ gulp.task('clean', function() { // clean all files in dist folder
 
 gulp.task('html', function(){
   return gulp.src('app/**/*.html')
-    .pipe(htmlreplace({
-      css: 'css/style.min.css',
-      js: 'js/script.min.js'
-    }))
-    .pipe(gulp.dest('dist'));	
+		.pipe(browserSync.stream());
+  // return gulp.src('app/**/*.html')
+  //   .pipe(htmlreplace({
+  //     css: 'css/style.min.css',
+  //     js: 'js/script.min.js'
+  //   }))
+  //   .pipe(gulp.dest('dist'));	
 });
 
 gulp.task('img', () => {
@@ -29,7 +40,7 @@ gulp.task('img', () => {
 });
 
 gulp.task('css', () => {
-  return gulp.src(['app/css/multirange.css', 'app/css/style.css'])
+  return gulp.src(['app/css/fonts.css', 'app/css/multirange.css', 'app/css/style.css'])
     .pipe(sourcemaps.init())
     .pipe(concat('style.min.css'))
     .pipe(cssnano())
@@ -46,7 +57,22 @@ gulp.task('js-babel', () => { // compile js into compatible version
     .pipe(gulp.dest('app/js'));
 });
 
+// watch app
+gulp.task('default', gulp.series(gulp.parallel('browser-sync', (done) => {
+	// gulp.watch('app/sass/**/*+(.sass|scss)', gulp.series('sass'));
+	// gulp.watch('app/js/**/*.js', gulp.series('js'));
+	gulp.watch('app/**/*.html', gulp.series('html'));
+	done();
+})));
+
+// build of distribution
 gulp.task('build', gulp.series(['clean', gulp.parallel(['html', 'img', 'css', 'js-babel'])], (done) => {
+  gulp.src('app/**/*.html')
+    .pipe(htmlreplace({
+      css: 'css/style.min.css',
+      js: 'js/script.min.js'
+    }))
+    .pipe(gulp.dest('dist'));	
   gulp.src(['app/js/multirange.js', 'app/js/clipboard.min.js', 'app/js/app.es5.js'])
     .pipe(sourcemaps.init())
     .pipe(concat('script.min.js'))
